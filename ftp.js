903ac9904fa3a0634aabdfb78d90865b8f9fd7ed
BatmanBruc <pupkinandroit@gmail.com>
2016-12-27 17:44:46 +1000

    sdgf

---
+++ ftp.js
@@ -0,0 +1,20 @@
+var Client = require('ssh2-sftp-client');
+var sftp = new Client();
+var ftp = function() {
+	sftp.connect({
+	    host: '192.168.111.103',
+	    port: '22',
+	    username: 'root',
+	    password: '12071995'
+	}).then(() => {
+	    return sftp.put(__dirname+'/public/style/style-mini.css','/var/www/r999111/data/www/999111.ru/templates/999111/css/public/style/style-mini.css', false);
+	}).then((data) => {
+		console.log('отправлено');
+	    sftp.end();
+	}).catch((err) => {
+	    console.log(err, 'catch error');
+	});
+}
+
+module.exports = ftp;
+	
--- pack.js
+++ pack.js
@@ -1,32 +1,38 @@
 var fs = require('fs');
 var CleanCSS = require('clean-css');
+var ftp = require('./ftp')
 
 
 var pack = function() {
-	if(fs.existsSync(__dirname+'/public/style.css'))fs.unlinkSync(__dirname+'/public/style.css');
+	if(fs.existsSync(__dirname+'/public/style/style.css'))fs.unlinkSync(__dirname+'/public/style/style.css');
 	var files = fs.readdirSync(__dirname+'/public');
 	console.log(files);
 
 	var styleFile = '';
 	for (var i = 0; i < files.length; i++) {
 		if(files[i]==='style.css')continue;
-		if(files[i].split('.')[1] === 'less'){
+		if(files[i].split('.')[1] === 'css'){
 			console.log(files[i]);
 			fileInclud = fs.readFileSync('./public/' + files[i], 'utf8');
 			styleFile += fileInclud;
 		}
 	}
 
-	var writer = fs.createWriteStream('public/style.css', {flags: 'w'});
-	writer.write(styleFile);
-
-	var minified
-	setTimeout(function(){
-		var source = fs.readFileSync('./public/style.css', 'utf8');;
+	var writer = fs.createWriteStream('public/style/style.css', {flags: 'w'});
+	console.log(styleFile);
+	writer.on('finish',function() {
+		var source = fs.readFileSync('./public/style/style.css', 'utf8');;
 		minified = new CleanCSS().minify(source).styles;
-		var writer = fs.createWriteStream('public/style.mini.css', {flags: 'w'});
-		writer.write(minified);
-	},200)
+		var writer = fs.createWriteStream('public/style/style-mini.css', {flags: 'w'});
+
+		writer.on('finish',function () {
+			ftp();
+		})
+		writer.end(minified);
+		console.log('Файл записан!');
+	});
+	console.log('styleFile');
+	writer.end(styleFile);
 	
 }
 
